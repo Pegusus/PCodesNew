d9882e1e7cb2f6697b7127c82002cacaf8a19ffe
Pegasus <aniscima@gmail.com>
2020-04-30 12:19:37 +0530

    Modified segment tree

--- Day16/main.cpp
+++ Day16/main.cpp
@@ -1,77 +1,67 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-void buildTree(int* arr,int* tree,int start,int end,int treenode){
-    if(start==end){
-        tree[treenode] = arr[start];
+void buildSegmentTree(int* arr,int* tree,int si,int ei,int treenode){
+    if(si == ei){
+        tree[treenode] = arr[si];
         return;
     }
-    int mid = start + (end-start)/2;
-    buildTree(arr,tree,start,mid,2*treenode);
-    buildTree(arr,tree,mid+1,end,2*treenode+1);
+    int mid = si + (ei-si)/2;
+    buildSegmentTree(arr,tree,si,mid,2*treenode+1);
+    buildSegmentTree(arr,tree,mid+1,ei,2*treenode+2);
 
-    tree[treenode] = tree[2*treenode] + tree[2*treenode + 1];
+    tree[treenode] = tree[2*treenode+1] + tree[2*treenode+2];
 }
-void updateTree(int* arr,int* tree,int start,int end,int treenode,int idx,int value){
-    if(start==end)
-    {
-        arr[idx] =  value;
-        tree[treenode] = value;
+void updateTree(int* arr,int* tree,int si,int ei,int treenode,int idx,int val)
+{
+    if(si==ei){
+        arr[idx] = val;
+        tree[treenode] = val;
         return;
     }
-    int mid = start + (end-start)/2;
+    int mid = si + (ei-si)/2;
 
-    if(idx<mid)
-        updateTree(arr,tree,start,mid,2*treenode,idx,value);
+    if(idx<=mid)
+        updateTree(arr,tree,si,mid,2*treenode+1,idx,val);
     else
-        updateTree(arr,tree,mid+1,end,2*treenode+1,idx,value);
+        updateTree(arr,tree,mid+1,ei,2*treenode+2,idx,val);
 
-    tree[treenode] = tree[2*treenode] + tree[2*treenode+1];
+    tree[treenode] = tree[2*treenode+1] + tree[2*treenode+2];
 }
-int querry(int* tree,int start,int end,int treenode,int left,int right){
-    //completely outside
-    if(end<left || start>right)
+int querry(int* tree,int si,int ei,int treenode,int l,int r){
+    if(ei<l || si>r)
         return 0;
-    //completely inside
-    if(start>=left && end<=right)
+    if(si>=l&&ei<=r)
         return tree[treenode];
-    //partially
-    int mid = start + (end-start)/2;
-    int ans1 = querry(tree,start,mid,2*treenode,left,right);
-    int ans2 = querry(tree,mid+1,end,2*treenode+1,left,right);
+    int mid = si + (ei-si)/2;
+
+    int ans1 = querry(tree,si,mid,2*treenode+1,l,r);
+    int ans2 = querry(tree,mid+1,ei,2*treenode+2,l,r);
 
     return ans1+ans2;
 }
-void display(int* arr)
-{
-    int n = sizeof(arr)/sizeof(arr[0]);
-    cout<<"array: ";
-    for(int i = 0;i<n;i++)
-        cout<<arr[i]<<" ";
-}
 int main()
 {
-    int arr[] = {1,2,5,6,7,9};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int arr[] = {1,3,5,7,9,6,2};
 
+    int n = sizeof(arr)/sizeof(arr[0]);
     int* tree = new int[4*n];
     for(int i = 0;i<4*n;i++)
         tree[i] = INT_MIN;
-
-    buildTree(arr,tree,0,5,1);
-
-    for(int i = 1;i<4*n;i++)
+    buildSegmentTree(arr,tree,0,n-1,0);
+    for(int i = 0;i<4*n;i++)
+    {
         if(tree[i]!=INT_MIN)
-        cout<<tree[i]<<" ";
+            cout<<i<<"-"<<tree[i]<<" ";
+    }
+    updateTree(arr,tree,0,n-1,0,1,3);
     cout<<endl;
-    updateTree(arr,tree,0,5,1,0,2);
-    display(arr);
-    cout<<endl;
-    for(int i = 1;i<4*n;i++)
+    for(int i = 0;i<4*n;i++)
+    {
         if(tree[i]!=INT_MIN)
-        cout<<tree[i]<<" ";
+            cout<<i<<"-"<<tree[i]<<" ";
+    }
     cout<<endl;
-    int ans = querry(tree,0,5,1,2,4);
-    cout<<ans;
+    cout<<querry(tree,0,n-1,0,1,3);
     return 0;
 }
